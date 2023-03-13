5edeccd5278dd86bad72c66eebb16a345bb7b39f
obijohnkanobi <119489622+obijohnkanobi@users.noreply.github.com>
2023-03-13 15:16:17 +0100

    xxx

---
+++ src/main/java/com/example/azurebegynderprojekt/AzureBegynderProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.azurebegynderprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureBegynderProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureBegynderProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azurebegynderprojekt/AzureBegynderProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurebegynderprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBegynderProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
